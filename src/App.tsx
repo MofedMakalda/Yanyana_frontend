acf730bff12b46e86c36c72b6d34c1909eeb1d1e
Mofedmakalda <mofedmk1@gmal.com>
2025-01-19 22:13:41 +0300

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -118,6 +118,7 @@ const Layout = () => {
       <Routes>
         {/* Admin Routes */}
         <Route path="/admin/login" element={<Login />} />
+        <Route path="/admin/register" element={<Register />} />
         {/* Public Routes */}
         <Route path="/" element={<HomePage />} />
         <Route path="/cruise" element={<Cruises />} />
@@ -129,7 +130,6 @@ const Layout = () => {
         <Route path="/contact" element={<ContactPage/>} />
         {/* Protected Admin Routes */}
         <Route element={<ProtectedRoute />}>
-          <Route path="/admin/register" element={<Register />} />
           <Route path="/admin/dashboard" element={<AdminDashboard />} />
           <Route path="/admin/cities" element={<CityManagement />} />
           <Route path="/admin/cruises" element={<CruiseManagement />} />
